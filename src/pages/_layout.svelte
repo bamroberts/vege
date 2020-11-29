518e0e341304a75821b732db8c5b200795c2e50b
Ben Roberts <iam@bamroberts.com>
2020-11-29 15:36:59 +1300

    first commit

---
+++ src/pages/_layout.svelte
@@ -0,0 +1,26 @@
+<!-- routify:options preload="proximity" -->
+<style>
+    header {
+        display:grid;
+        height: 100vh;
+        background-color:aliceblue;
+        background: url('/images/vegetables-wallpaper-20.jpg');
+        background-repeat: no-repeat;
+        background-size: cover;
+    }
+
+    h1 {
+        align-self: top;
+        text-transform:uppercase;
+        font-size:max(18vmin, 10em);
+        width:100px;
+        margin:0;
+        line-height:1;
+        color:#3F6D49;
+        text-shadow:2px 2px 30px rgba(255,255,255.1), 1px 1px 2px rgba(0,0,0,0.4);
+    }
+</style>
+<header>
+    <h1>My Vege Garden</h1>
+</header>
+<slot />
---
+++ src/pages/index.svelte
@@ -0,0 +1,13 @@
+<script>
+    import RoutifyIntro from './example/_components/RoutifyIntro.svelte'
+    import { metatags } from '@roxi/routify'
+    metatags.title = 'My Vege Garden'
+    metatags.description = 'A subscripotion service for veggies to grow your self'
+</script>
+<!-- 
+<div>
+    <img src="/images/57654451be1169988ac17a8b4b2954aa.jpg" alt="Basket of veg"/>
+<p>
+    My Vege Garder is a subscripton based service to deliver both monthly fresh seedlings and the quiptment needed to grow them.
+</p>
+</div> -->
