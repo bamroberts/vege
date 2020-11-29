16fb040a071da3b11388f95e240026f320051fad
Ben Roberts <iam@bamroberts.com>
2020-11-29 17:15:01 +1300

    second commit

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,15 +1,25 @@
 <!-- routify:options preload="proximity" -->
 <style>
+    :global(body) {
+        background-color:#3F6D49;
+        margin:0;
+        padding:0;
+    }
     header {
         display:grid;
-        height: 100vh;
+        height:100vh;
+        min-height:100%;
         background-color:aliceblue;
         background: url('/images/vegetables-wallpaper-20.jpg');
         background-repeat: no-repeat;
         background-size: cover;
+        grid-template-areas: 'a'
+        'b';
+        grid-template-rows: 1fr min-content;
     }
 
     h1 {
+        grid-area: a;
         align-self: top;
         text-transform:uppercase;
         font-size:max(18vmin, 10em);
@@ -17,10 +27,34 @@
         margin:0;
         line-height:1;
         color:#3F6D49;
-        text-shadow:2px 2px 30px rgba(255,255,255.1), 1px 1px 2px rgba(0,0,0,0.4);
+        text-shadow: 1px 1px 2px rgba(0,0,0,0.4), 2px 2px 150px rgba(255,255,255.1), 2px 2px 50px rgba(255,255,255.1);
     }
+    h3 {
+        grid-area: a;
+    color: #E48B3E;
+    font-size: 3em;
+    width: 300px;
+    border-bottom: 0.4em solid #E48B3E;
+    right: 0;
+    justify-self: end;
+    text-align: right;
+    margin-right: 2em;
+    align-self: baseline;
+    padding-bottom: 20px;
+    overflow: visible;
+    padding-top: 00;
+    margin-top: 0;
+    margin-right: 3vw;
+    text-shadow: 1px 1px 2px rgba(0,0,0,0.4), 2px 2px 150px rgba(255,255,255.1), 2px 2px 50px rgba(255,255,255.1);
+
+    }
+
+    h3 span {display:block;}
 </style>
 <header>
     <h1>My Vege Garden</h1>
+    <h3>Exceed <span>in Gardening</span></h3>
+    <slot />
 </header>
-<slot />
+
+
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -3,7 +3,95 @@
     import { metatags } from '@roxi/routify'
     metatags.title = 'My Vege Garden'
     metatags.description = 'A subscripotion service for veggies to grow your self'
+
+    let sent = false;
 </script>
+<style>
+    form {
+        font-size:2vmin;
+        width:80ch;
+        grid-area:b;
+        background-color:rgba(255,255,255,0.8);
+        padding:20px;
+        border-radius:10px;
+        place-self: center;
+        margin-bottom:1em;
+
+        font-family: sans-serif;
+
+    }
+
+    p {
+        font-size:1.5em;
+        font-weight: bold;
+    text-align: center;
+    }
+
+    label {font-weight:bold;
+    margin:40px 0px 20px;
+    font-size:2em;
+    }
+
+    div {
+        display:flex;
+        align-items: center;
+        justify-content: center;
+        
+        }
+
+        hr {
+            background-color: #3F6D49;
+            height:3px;
+            width:40ch;
+            padding-bottom:10px;
+            margin-bottom: 10px;;
+        }
+
+        input {flex-grow: 1;}
+
+        input, button {
+            font-size: min(2em, 4vw);
+        }
+
+        button {background-color:#E48B3E;
+        color:white;
+        font-weight: bold;
+        text-transform:uppercase;
+        text-shadow:0 0 1px black;
+        cursor: pointer;}
+
+        p.sent {
+            text-align:left;
+            font-weight:100;
+
+        } 
+
+        p.sent b {
+            color:#E48B3E;
+        }
+</style>
+<form on:submit|preventDefault={()=>sent=true}>
+    <p>
+        My Vege Garden is a subscripton based service to deliver both monthly fresh seedlings and the quiptment needed to grow them.
+    </p>
+    <hr >
+    
+    {#if sent} 
+
+   
+    <p class="sent"> <strong>Thanks!</strong><br />Great to have you on board. We will be in contact when our new vege growing system is closer to launch! Thanks for being a supporter of <b>My Vege Box</b>!</p>
+    {:else}
+    <span>
+        <label for=email>Sign up to find out more</label>
+        <div>
+        
+        <input type=email name=email />
+        <button>Signup</button>
+        </div>
+    </span>
+    {/if}
+
+</form>
 <!-- 
 <div>
     <img src="/images/57654451be1169988ac17a8b4b2954aa.jpg" alt="Basket of veg"/>
